384746fb072f87ffea2e2ae3814693fac631cc3c
Balakrishna.G <70707659+geelabalakrishna@users.noreply.github.com>
2024-02-13 17:28:17 +0530

    Update centos-maven-install.sh

--- centos-maven-install.sh
+++ centos-maven-install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash #shebang
-cd /tmp/
+cd /opt/
 wget https://dlcdn.apache.org/maven/maven-3/3.9.6/binaries/apache-maven-3.9.6-bin.tar.gz
 APACHE=$(ls -l | grep apache-maven | awk '{print $9}')
 tar vxfz $APACHE
 rm -rf $APACHE
 MAVEN_DIR=$(ls -ltr | grep apache-maven | awk '{print $9}')
-echo "export MAVEN_HOME=/tmp/$MAVEN_DIR" >> $HOME/.bash_profile
+echo "export MAVEN_HOME=/opt/$MAVEN_DIR" >> $HOME/.bash_profile
 echo 'export PATH=$MAVEN_HOME/bin:$PATH' >> $HOME/.bash_profile
 source $HOME/.bash_profile
 echo "maven install sucessfully"
-mvn --version
+
 
